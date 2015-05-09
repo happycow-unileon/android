3b650117b82b808127ede9d22f65e1625b4b7f5b
Dorian Hawkmoon <dorian.cadenas@gmail.com>
2015-05-09 20:52:24 +0200

    Olvidado poner un método XD

--- app/src/main/java/es/unileon/happycow/controller/vetgui/AddFarmFragment.java
+++ app/src/main/java/es/unileon/happycow/controller/vetgui/AddFarmFragment.java
@@ -75,7 +75,7 @@ public class AddFarmFragment extends Fragment {
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                saveFarm();
             }
         });
         return rootView;
