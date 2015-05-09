d7ceafac6d2213763a8897d4d32f13617ce5acae
Dorian Hawkmoon <dorian.cadenas@gmail.com>
2015-05-09 20:50:16 +0200

    Integrado el modelo en AddFarmFragment, sin testear

--- app/src/main/java/es/unileon/happycow/controller/vetgui/AddFarmFragment.java
+++ app/src/main/java/es/unileon/happycow/controller/vetgui/AddFarmFragment.java
@@ -1,19 +1,30 @@
 package es.unileon.happycow.controller.vetgui;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 import es.unileon.happycow.R;
+import es.unileon.happycow.database.Database;
+import es.unileon.happycow.handler.IdFarm;
+import es.unileon.happycow.handler.IdHandler;
+import es.unileon.happycow.model.Farm;
 
 public class AddFarmFragment extends Fragment {
-/*
-    private AddFarmView addFarmView;
+    private TextView etFarmerName, etFarmerID, etFarmID, etFarmName, etCowsNum, etAddressFarm, etOtherData;
+    private IdHandler idFarm;
+    //private AddFarmView addFarmView;
+
+    /*
     private static class AddFarmView implements View.OnFocusChangeListener {
-        TextView etFarmerName, etFarmerID, etFarmID, etFarmName, etCowsNum;
+
 
         public AddFarmView(Activity activity){
             etFarmerName = (TextView)activity.findViewById(R.id.editTextFarmerName);
@@ -31,7 +42,6 @@ public class AddFarmFragment extends Fragment {
             etCowsNum = (TextView)activity.findViewById(R.id.editTextCowsNum);
             etCowsNum.setOnFocusChangeListener(this);
 
-
         }
 
         @Override
@@ -47,12 +57,27 @@ public class AddFarmFragment extends Fragment {
 */
 
 
-    public AddFarmFragment(){}
+    public AddFarmFragment(){
+        idFarm=null;
+
+}
+
+    public void setIdFarm(IdHandler idFarm) {
+        this.idFarm = idFarm;
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_add, container, false);
+        Button add=(Button)rootView.findViewById(R.id.buttonAddFarm);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
         return rootView;
     }
 
@@ -61,5 +86,89 @@ public class AddFarmFragment extends Fragment {
         super.onStart();
         //addFarmView = new AddFarmView(getActivity());
 
+    }
+
+
+    public void saveFarm(){
+        Farm farm;
+        if(controlFarm()){
+            if(idFarm!=null){
+                farm= Database.getInstance(null).getFarm(idFarm);
+                farm.setAddress(etAddressFarm.getText().toString());
+                farm.setCowNumber(Integer.parseInt(etCowsNum.getText().toString()));
+                farm.setDniFarmer(etFarmerID.getText().toString());
+                farm.setFarmName(etFarmName.getText().toString());
+                farm.setFarmerName(etFarmerName.getText().toString());
+                farm.setOtherData(etOtherData.getText().toString());
+            }else{
+                IdHandler identificationfarm=new IdFarm(Database.getInstance(null).nextIdFarm());
+                farm=new Farm(identificationfarm, etFarmName.getText().toString(), etFarmID.getText().toString(),
+                        etAddressFarm.getText().toString(), etFarmerName.getText().toString(),
+                        etFarmerID.getText().toString(), Integer.parseInt(etCowsNum.getText().toString()),
+                        Database.getInstance(null).getUser().getId(), etOtherData.getText().toString());
+            }
+
+            if(idFarm!=null){
+                if(!Database.getInstance(null).modifiedFarm(farm)){
+                    Toast toast1 =
+                            Toast.makeText(this.getActivity().getApplicationContext(),
+                                    "Errores al guardar la granja, inténtelo de nuevo", Toast.LENGTH_SHORT);
+
+                    toast1.show();
+
+                }else{
+                    //father.seeListFarm();
+                    //volver a la lista de granjas una vez añadido
+                }
+            }else{
+                if(!Database.getInstance(null).newFarm(farm)){
+                    Toast toast1 =
+                            Toast.makeText(this.getActivity().getApplicationContext(),
+                                    "Errores al guardar la granja, inténtelo de nuevo", Toast.LENGTH_SHORT);
+
+                    toast1.show();
+                }else{
+                    //father.seeListFarm();
+                    //volver a la lista de granjas una vez añadido
+                }
+            }
+        }
+    }
+
+    private boolean controlFarm(){
+        boolean correct=true;
+        //StringBuilder errores=new StringBuilder();
+        if(etFarmName.getText().toString().trim().compareToIgnoreCase("")==0){
+            correct=false;
+            etFarmName.setError("Debe dar un nombre a la granja.\n" +
+                    "Use el identificador de la granja como nombre si no se le ocurre uno.");
+            //errores.append("\nDebe dar un nombre a la granja.\nUse el identificador de la granja como nombre si no se le ocurre uno.");
+        }
+
+        if(etCowsNum.getText().toString().trim().compareTo("")==0){
+            correct=false;
+            etCowsNum.setError("Debe introducir el número de vacas exactas/aproximadas que tiene la granja.");
+            //errores.append("\nDebe introducir el número de vacas exactas/aproximadas que tiene la granja.");
+        }else if(!isNumeric(etCowsNum.getText().toString())){
+            correct=false;
+            etCowsNum.setError("Debe introducir un número entero de vacas.");
+            //errores.append("\nDebe introducir un número entero de vacas.");
+        }
+
+        //if(!correct){
+            //JOptionPane.showMessageDialog(panel, errores, "Mala entrada", JOptionPane.WARNING_MESSAGE);
+        //}
+        //cambiar por borrar el setError cuando se recupera el foco en el campo
+
+        return correct;
+    }
+
+    private boolean isNumeric(String cadena) {
+        try {
+            Integer.parseInt(cadena);
+            return true;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
     }
 }
